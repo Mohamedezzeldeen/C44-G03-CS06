5c07d99b712690672c4bc1c86c8ae57aa9676e88
mohamed_ezz <mohamed_ezz@gmail.com>
2025-08-07 09:27:44 +0300

    Q2

--- C44-G03-CS06/Program.cs
+++ C44-G03-CS06/Program.cs
@@ -14,6 +14,14 @@ namespace C44_G03_CS06
     Sunday
 }
 
+    internal enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+
 
     internal class Program
     {
@@ -24,6 +32,34 @@ namespace C44_G03_CS06
             //foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
             //{
             //    Console.WriteLine(day);
+            //}
+            #endregion
+
+            #region 2-Create an Enum called "Season" with the four seasons (Spring, Summer, Autumn, Winter) as its members. Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
+            //Console.Write("Please Enter Season Nmae : ");
+            //Season SeasonName;
+            //bool Flag;
+            //Flag = Enum.TryParse(Console.ReadLine(), out SeasonName);
+
+
+            //Console.Clear();
+            //if (Flag) 
+            //{
+            //    switch (SeasonName)
+            //    {
+            //        case Season.Spring:
+            //            Console.WriteLine("March to May");
+            //            break;
+            //        case Season.Summer:
+            //            Console.WriteLine("June to August");
+            //            break;
+            //        case Season.Autumn:
+            //            Console.WriteLine("September to November");
+            //            break;
+            //        case Season.Winter:
+            //            Console.WriteLine("December to February");
+            //            break;
+            //    }
             //}
             #endregion
         }
