dfb3f44109cd9c90c806574027537d82d4d95e2e
mohamed_ezz <mohamed_ezz@gmail.com>
2025-08-07 09:25:50 +0300

    Q1

--- C44-G03-CS06/Program.cs
+++ C44-G03-CS06/Program.cs
@@ -3,12 +3,29 @@ using System.Drawing;
 
 namespace C44_G03_CS06
 {
+    internal enum WeekDays
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
 
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            #region 1-Create an Enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this Enum.
+
+            //foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            //{
+            //    Console.WriteLine(day);
+            //}
+            #endregion
         }
     }
 }
