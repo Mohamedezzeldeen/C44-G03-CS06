ac4729b573b2ee351ef4b37404ce656194da0eb4
mohamed_ezz <mohamed_ezz@gmail.com>
2025-08-07 09:24:03 +0300

    Add project files.

---
+++ C44-G03-CS06/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+
+namespace C44_G03_CS06
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
